2015ba07e94e06f10e33e6c1d6042aaf4063bff5
RicardoPBJ <rbrumjunio@gmail.com>
2024-07-09 19:52:06 -0300

    iniciando o projeto

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { Form } from "@/components/ui/form";
+import { Select } from "@/components/ui/select";
+import Image from "next/image";
+
+export default function Home() {
+    return (
+    <main >
+      <div>.</div>
+    </main>
+  );
+}
